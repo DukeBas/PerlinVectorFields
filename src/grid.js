e6cb8554ebd1427578710c3bcfeb8fc011d761a2
DukeBas <duke.bas@gmail.com>
2021-06-14 16:51:54 +0200

    Fixed .gitignore

---
+++ src/grid.js
@@ -0,0 +1,64 @@
+var Grid = (function () {
+    function Grid(numHorizontalCells, numVerticalCells) {
+        this.hor = numHorizontalCells;
+        this.vert = numVerticalCells;
+        this.cells = [];
+        for (var i = 0; i < numHorizontalCells; i++) {
+            this.cells[i] = [];
+        }
+        this.generateNoisedCells();
+    }
+    Grid.prototype.generateRandomCells = function () {
+        for (var i = 0; i < numHorizontalCells; i++) {
+            this.cells[i] = [];
+            for (var j = 0; j < numHorizontalCells; j++) {
+                var direction = random(0, 360);
+                this.cells[i][j] = { dir: direction };
+            }
+        }
+    };
+    Grid.prototype.generateNoisedCells = function () {
+        for (var i = 0; i < numHorizontalCells; i++) {
+            this.cells[i] = [];
+            for (var j = 0; j < numHorizontalCells; j++) {
+                var direction = noise(i / 25, j / 25) * 360;
+                this.cells[i][j] = { dir: direction };
+            }
+        }
+    };
+    Grid.prototype.getGrid = function () {
+        return this.cells;
+    };
+    Grid.prototype.draw = function (state) {
+        var cellWidth = windowWidth / this.hor;
+        var cellHeight = windowHeight / this.vert;
+        var cellMin = min(cellWidth, cellHeight);
+        switch (state) {
+            case "vector":
+                for (var i = 0; i < this.hor; i++) {
+                    for (var j = 0; j < this.vert; j++) {
+                        var cell = this.cells[i][j];
+                        var locX = i * cellWidth;
+                        var locY = j * cellHeight;
+                        line(locX + cellWidth / 2, locY + cellHeight / 2, locX + cellWidth / 2 * (1 + Math.cos(cell.dir * (Math.PI / 180))), locY + cellHeight / 2 * (1 + Math.sin(cell.dir * (Math.PI / 180))));
+                    }
+                }
+                break;
+            case "heatmap":
+                for (var i = 0; i < this.hor; i++) {
+                    for (var j = 0; j < this.vert; j++) {
+                        var cell = this.cells[i][j];
+                        var locX = i * cellWidth;
+                        var locY = j * cellHeight;
+                        push();
+                        fill(map(cell.dir, 0, 360, 0, 255));
+                        rect(locX, locY, cellWidth, cellHeight);
+                        pop();
+                    }
+                }
+                break;
+        }
+    };
+    return Grid;
+}());
+//# sourceMappingURL=TS/grid.js.map
---
+++ src/sketch.js
@@ -0,0 +1,24 @@
+var numHorizontalCells = 50;
+var numVerticalCells = 40;
+var grid;
+function setup() {
+    var canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
+    frameRate(5);
+    background(255);
+    grid = new Grid(numHorizontalCells, numVerticalCells);
+}
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+function draw() {
+    background(255);
+    var state = getCurrentState();
+    grid.draw(state);
+}
+function getCurrentState() {
+    var selector = document.getElementById('state-selector');
+    return selector.value;
+}
+//# sourceMappingURL=TS/sketch.js.map
