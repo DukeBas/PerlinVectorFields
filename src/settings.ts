3841c13eba23d1b65a9070d4caeedf024339c43d
DukeBas <duke.bas@gmail.com>
2021-06-17 00:33:37 +0200

    Time shift

--- src/settings.ts
+++ src/settings.ts
@@ -8,12 +8,14 @@ const settings = {
   fieldStrength: 0.5, // how strong the acceleration is of a vector to a particle
   noiseDifference: 2, // how much difference each step of noise makes (ex: 1, 0 to 1 gets mapped to 0 to 360 degrees,)
                       // (but with a value of 2, 0,1 gets mapped to 0, 720 but it loops at 360, so values are more spread)
-  noiseTimeResistance: 50, // how much each time step changes the angles. higher = less change 
-  slowFrameRate: 5,
+  noiseTimeResistance: 500, // how much each time step changes the angles. higher = less change 
+  slowFrameRate: 30,
   maxFrameRate: 60,
   fpsBufferSize: 4, // higher = smoother
+  timeShift: false,
 }
 
 //TODO
 // noise seed (+ show / alter in sidebar)
 // noise detail (perlin noise octaves)
+// noise time resistance slider
